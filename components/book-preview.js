da58f9588b9ad538aab32415f5c54d9c725c489a
katlego Maphango <mphangokatlego08@gmail.com>
2023-06-14 06:01:41 +0200

    updated book preview

--- components/book-preview.js
+++ components/book-preview.js
@@ -3,61 +3,63 @@
 const template = document.createElement("template")
 
 template.innerHTML = /* Html */ `
-    .preview {
-        border-width: 0;
-        width: 100%;
-        font-family: Roboto, sans-serif;
-        padding: 0.5rem 1rem;
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        text-align: left;
-        border-radius: 8px;
-        border: 1px solid rgba(var(--color-dark), 0.15);
-        background: rgba(var(--color-light), 1);
-    }
-    
-    @media (min-width: 60rem) {
+    <style>
         .preview {
-        padding: 1rem;
+            border-width: 0;
+            width: 100%;
+            font-family: Roboto, sans-serif;
+            padding: 0.5rem 1rem;
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+            text-align: left;
+            border-radius: 8px;
+            border: 1px solid rgba(var(--color-dark), 0.15);
+            background: rgba(var(--color-light), 1);
         }
-    }
-    
-    .preview_hidden {
-        display: none;
-    }
-    
-    .preview:hover {
-        background: rgba(var(--color-blue), 0.05);
-    }
-    
-    .preview__image {
-        width: 48px;
-        height: 70px;
-        object-fit: cover;
-        background: grey;
-        border-radius: 2px;
-        box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
-        0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
-    }
-    
-    .preview__info {
-        padding: 1rem;
-    }
-    
-    .preview__title {
-        margin: 0 0 0.5rem;
-        font-weight: bold;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;  
-        overflow: hidden;
-        color: rgba(var(--color-dark), 0.8)
-    }
-    
-    .preview__author {
-        color: rgba(var(--color-dark), 0.4);
-    }
+        
+        @media (min-width: 60rem) {
+            .preview {
+            padding: 1rem;
+            }
+        }
+        
+        .preview_hidden {
+            display: none;
+        }
+        
+        .preview:hover {
+            background: rgba(var(--color-blue), 0.05);
+        }
+        
+        .preview__image {
+            width: 48px;
+            height: 70px;
+            object-fit: cover;
+            background: grey;
+            border-radius: 2px;
+            box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+            0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+        }
+        
+        .preview__info {
+            padding: 1rem;
+        }
+        
+        .preview__title {
+            margin: 0 0 0.5rem;
+            font-weight: bold;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;  
+            overflow: hidden;
+            color: rgba(var(--color-dark), 0.8)
+        }
+        
+        .preview__author {
+            color: rgba(var(--color-dark), 0.4);
+        }
+    </style>
 
     <div class="preview" data-preview>
         <img
@@ -84,9 +86,9 @@ class BookPreview extends HTMLElement {
     // @ts-ignore
     #elements = {
         /**
-         * @type {undefined | HTMLImageElement}
+        //  * @type {undefined | HTMLImageElement}
          */
-        dataPreview: undefined,
+        // dataPreview: undefined,
         /**
          * @type {undefined | HTMLImageElement}
          */
@@ -112,7 +114,7 @@ class BookPreview extends HTMLElement {
     connectedCallback() {
         this.#elements = {
             // @ts-ignore
-            dataPreview: this.#inner.querySelector('data-preview'),
+            // dataPreview: this.#inner.querySelector('data-preview'),
             // @ts-ignore
             img: this.#inner.querySelector('img'),
             // @ts-ignore
@@ -122,7 +124,7 @@ class BookPreview extends HTMLElement {
         }
 
         // @ts-ignore
-        this.#elements.dataPreview.src = this.#dataPreview
+        // this.#elements.dataPreview.dataset.data-preview = this.#dataPreview
         // @ts-ignore
         this.#elements.img.src = this.#src
         // @ts-ignore
